29f95c5d76d81ac7958d51a49980284e11092b62
EhteshamullHasan <120940754+EhteshamullHasan@users.noreply.github.com>
2023-12-31 09:29:09 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 123445
+let accountEmail = "ehtesham@gmail.com";
+var accPassword = "233423";
+accountCity = "jaipur";
+let accountName;
+
+accountEmail = "hasan@gmil.com"
+accPassword ="24512";
+accountCity = "Bengaluru";
+
+console.table(
+    [accountId, accountEmail,accPassword,accountCity,accountName])
